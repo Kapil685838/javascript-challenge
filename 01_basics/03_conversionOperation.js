4834f2ad0a65f9aef165e597cd29588867129f56
Kapil Chaudhary <108571992+Kapil685838@users.noreply.github.com>
2024-04-26 17:55:20 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,46 @@
+// let score = 33
+// let score = "33"
+// let score = "33abc"
+// let score = null
+// let score = undefined
+// let score = "Kapil"
+let score = true
+
+// const {score} = req.body
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggenIn = "";
+
+let booleanIsLoggenIn = Boolean(isLoggenIn);
+// console.log(booleanIsLoggenIn);
+// console.log(typeof isLoggenIn);
+
+
+// console.log("First ---> ", "" || "Nisha" || "Kapil");
+// console.log("Second ---> ", "Kapil" && false && "Nisha");
+
+// console.log(true ? "Kapil" : "Nisha");
+// console.log(false ? "Kapil" : "Nisha");
+
+
+// 1 => true; 0 => false
+// "" => false
+// "Kapil" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
