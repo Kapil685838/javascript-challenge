adaa74762f4adbfdf0f631e377baa12161293979
Kapil Chaudhary <108571992+Kapil685838@users.noreply.github.com>
2024-04-27 16:47:53 +0000

    Operations done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -62,11 +62,27 @@ let str1 = "hello"
 let str2 = " kapil"
 
 let str3 = str1 + str2
-console.log(str3)
+// console.log(str3)
 
-console.log("1" + 2)
-console.log(1 + "2")
-console.log("1" + 2 + 2)
-console.log(1 + 2 + "2")
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
 
+// console.log((3 + 4) * 5 % 3);
+
+
+// console.log(+true);
+// console.log(true+); // error
+// console.log(+"");
+
+let num1, num2, num3;
+
+num1 = num2 = num3 = 2 + 2;
+
+
+let gameCounter = 100;
+// gameCounter++;
+++gameCounter;
+console.log(gameCounter);
 
