cb7207921e94b3f567b708a97904716bec916f82
Kapil Chaudhary <108571992+Kapil685838@users.noreply.github.com>
2024-04-30 19:00:08 +0000

    scopes basics

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,19 @@
+let a = 300;
+// const b = 10;
+// var c = 300;
+
+if (true) {
+  let a = 10;
+  const b = 10;
+  //   var c = 30;
+  console.log("INNER: ", a);
+}
+
+// for (let i = 0; i < array.length; i++) {
+//     const element = array[i];
+    
+// }
+
+console.log(a);
+// console.log(b);
+// console.log(c);
