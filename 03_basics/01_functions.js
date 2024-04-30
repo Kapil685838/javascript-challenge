663651797c884d7ac7a93afe39a958e873ac040c
Kapil Chaudhary <108571992+Kapil685838@users.noreply.github.com>
2024-04-30 18:27:03 +0000

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -41,4 +41,35 @@ function loginUserMessage(username = "kap") {
 }
 
 // console.log(loginUserMessage("Kapil"))
-console.log(loginUserMessage())
+// console.log(loginUserMessage())
+
+function calculateCartPrice(val1, val2, ...num1) {
+    return num1;
+}
+
+// console.log(calculateCartPrice(200, 400, 500, 2000));
+
+const user = {
+    username: "Hitesh",
+    price: 199
+}
+
+function handleObject(anyObject) {
+    console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`)
+}
+
+// handleObject(user)
+handleObject({
+    username: "Nisha",
+    price: 399
+})
+
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray) {
+    return getArray[2]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 100, 600]));
