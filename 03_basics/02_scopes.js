5b04779f8ffb8898f2620ce8d13f063fbcc24f7e
Kapil Chaudhary <108571992+Kapil685838@users.noreply.github.com>
2024-05-02 17:06:31 +0000

    Scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -6,14 +6,66 @@ if (true) {
   let a = 10;
   const b = 10;
   //   var c = 30;
-  console.log("INNER: ", a);
+  // console.log("INNER: ", a);
 }
 
 // for (let i = 0; i < array.length; i++) {
 //     const element = array[i];
-    
+
 // }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+function one() {
+  const username = "kapil";
+
+  function two() {
+    const website = "github";
+    console.log("username ----> ", username);
+  }
+  
+  // console.log("website ----> ", website);
+  two();
+}
+
+// console.log("username ----> ", username);
+
+// one();
+
+if(true) {
+  const username = "kapil"
+
+  if(username === "kapil") {
+    const website = " github";
+    // console.log("username & website ----> ", username + website);
+  }
+
+  // console.log("website", website)
+}
+
+// console.log("username", username);
+
+
+// +++++++++++++++++++++ Interesting ++++++++++++++++++++
+
+// console.log(addOne(5))
+
+function addOne(num) {
+  return num + 1;
+}
+
+
+// addOne(5);
+
+console.log(addOne(5))
+
+
+const addTwo = function(num) {
+  return num + 2;
+}
+
+addTwo(5)
+
+
